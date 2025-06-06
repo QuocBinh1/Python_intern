286b8e523097367495bbc114c2f8e67d600199c9
QuocBinh1 <BinhPlayll@gmail.com>
2025-06-06 18:37:26 +0700

    update two

--- week3_Selenium/Search_Selenium.py
+++ week3_Selenium/Search_Selenium.py
@@ -2,17 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 driver = webdriver.Chrome()   #khởi động 
-driver.get("https://www.google.com/") #mở trang web google
+driver.get("https://www.facebook.com/") 
 
+# #tim phần tử bằng ID và nhập
+search_box = driver.find_element(By.ID,"email") 
+search_box.send_keys("binhplayll@gmail.com")
 
-#tim phần tử bằng class name
-search_box = driver.find_element(By.CLASS_NAME,"gLFyf")
-search_box.send_keys("việt nam mãi đỉnh")
+search_box = driver.find_element(By.ID,"pass") 
+search_box.send_keys("Benhi11@")
 
-
-#nhấn search
-search_button = driver.find_elements(By.CLASS_NAME, "gNO89b")[1]
+#nhấn vào nút đăng nhập
+search_button = driver.find_element(By.NAME, "login")
 search_button.click()
-
 time.sleep(20) 
 driver.quit()
