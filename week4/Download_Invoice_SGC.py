5ceb6f1e0bc71226f30b3002673be22b045370ee
binhp <binhplayll@gmail.com>
2025-06-23 13:10:39 +0700

    update

--- week4/Download_Invoice_SGC.py
+++ week4/Download_Invoice_SGC.py
@@ -2,32 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
- # Khởi động trình duyệt Chrome
-driver = webdriver.Chrome() 
 
-# Mở trang web
-driver.get("https://www.meinvoice.vn/tra-cuu")  
+def main():
+    #1 mở trinh duyệt: cấu hình download file selenium
+    driver = webdriver.Chrome()     
 
-# Tìm ô và nhập mã hoá đơn
-invoice_input = driver.find_element(By.ID,"txtCode").send_keys("B1HEIRR8N0WP")
-try:
-    # bấm nút tìm kiếm
-    input_search = driver.find_element(By.ID,"btnSearchInvoice").click()
-    time.sleep(2) 
+    driver.get("https://www.meinvoice.vn/tra-cuu")  
 
-    # bấm nút tải xuống và hiện ra 2 lựa chọn
-    download_button = driver.find_element(By.CLASS_NAME,"res-btn.download").click()
-    time.sleep(2) 
+    # nhập mã hoá đơn vào ô tương ứng
+    # 2 hàm nhập mã tra cứu
+    invoice_input = driver.find_element(By.ID,"txtCode").send_keys("B1HEIRR8N0WP")
+    try:
+        # hàm thực hiện hành động tìm kiếm
+        # 3 hàm thực hiện tìm kiếm 
+        input_search = driver.find_element(By.ID,"btnSearchInvoice").click()
+        time.sleep(2) 
 
-    # tìm lựa chọn tải bằng pdf
-    pdf_option = driver.find_element(By.XPATH, "//div[contains(@class,'txt-download-pdf')]").click()
+        # xử lý kết quả tìm kiếm 
+        # 4 hàm thực hiện tải xuống hoá đơn
+        download_button = driver.find_element(By.CLASS_NAME,"res-btn.download").click()
+        time.sleep(2) 
 
-except Exception as e:
-    print("Không tìm thấy hoá đơn")
-    
+        # tải hoá đơn điện tử (PDF) về hệ thống cục bộ
+        # 5 tải hoá đơn , tra cứu thành công
+        pdf_option = driver.find_element(By.XPATH, "//div[contains(@class,'txt-download-pdf')]").click()
 
-# duy trì hiện trang
-time.sleep(500) 
+    except Exception as e:
+        print("Không tìm thấy hoá đơn")
+        
 
-# đóng trình duyệt
-driver.quit() 
+    # duy trì hiện trang
+    time.sleep(500) 
+
+    # đóng trình duyệt
+    driver.quit() 
